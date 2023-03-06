5a2f0969aebe24c6e456116171e3070cb14d12fd
ZylalMinollari <zminollari@gmail.com>
2023-03-06 17:49:32 +0100

    Resize image

--- app/Http/Controllers/V1/ImageManipulationController.php
+++ app/Http/Controllers/V1/ImageManipulationController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Http\Controllers\Controller;
+use App\Models\Album;
+use Illuminate\Support\Str;
 use App\Models\ImageManipulation;
-use App\Http\Requests\StoreImageManipulationRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ResizeImageRequest;
 use App\Http\Requests\UpdateImageManipulationRequest;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 
 class ImageManipulationController extends Controller
 {
@@ -19,46 +23,51 @@ class ImageManipulationController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreImageManipulationRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreImageManipulationRequest $request)
+    public function resize(ResizeImageRequest $request)
     {
-        //
+        $all = $request->all();
+        $image = $all['image'];
+        unset($all['image']);
+        $data = [
+            'image' => ImageManipulation::TYPE_RESIZE,
+            'data' => json_encode($all),
+            'user_id' => null,
+        ];
+
+        if(isset($all['album_id'])) {
+
+            //TODO
+
+            $data['album_id'] = $all['album_id'];
+        }
+
+        $dir = 'images/' . Str::random() . '/';
+        $absolutePath = public_path($dir);
+        File::makeDirectory($absolutePath);
+
+        if($image instanceof UploadedFile) {
+            $data['name'] = $image->getClientOriginalName();
+            $filename = pathinfo($data['name'], PATHINFO_FILENAME);
+            $extension = $image->getClientOriginalExtension();
+
+            $image->move($absolutePath, $data['name']);
+        } else {
+            $data['name'] = pathinfo($image, PATHINFO_BASENAME);
+            $filename = pathinfo($image, PATHINFO_FILENAME);
+            $extension = pathinfo($$image, PATHINFO_EXTENSION);
+
+            copy($image, $absolutePath . $data['name']);
+        }
+        $data['path'] = $dir . $data['name'];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ImageManipulation  $imageManipulation
-     * @return \Illuminate\Http\Response
-     */
     public function show(ImageManipulation $imageManipulation)
     {
         //
     }
+    public function byAlbum(Album $album) {
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateImageManipulationRequest  $request
-     * @param  \App\Models\ImageManipulation  $imageManipulation
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateImageManipulationRequest $request, ImageManipulation $imageManipulation)
-    {
-        //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ImageManipulation  $imageManipulation
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(ImageManipulation $imageManipulation)
     {
         //
